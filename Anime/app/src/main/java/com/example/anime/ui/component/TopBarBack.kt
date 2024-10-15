010b242b6bbfe23e8fa76f0547dc90679dbf626b
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-10-15 16:22:42 +0800

    feat: navigateUp topbar

--- Anime/app/src/main/java/com/example/anime/ui/component/TopBarBack.kt
+++ Anime/app/src/main/java/com/example/anime/ui/component/TopBarBack.kt
@@ -29,7 +29,7 @@ fun TopBarBack(
         },
         navigationIcon = {
             IconButton(onClick = {
-
+                navController.navigateUp()
             }) {
                 Icon(
                     imageVector = Icons.Default.ArrowBack,
